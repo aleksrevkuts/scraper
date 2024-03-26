fcf4b198878af6c534d3aef0367a69b066f72806
aleksrevkuts <124285129+aleksrevkuts@users.noreply.github.com>
2024-03-27 01:07:48 +0200

    Add files via upload

---
+++ scraper.mjs
@@ -0,0 +1,33 @@
+import axios from "axios";
+import * as cheerio from 'cheerio';
+import fs from 'node:fs';
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+if(!fs.existsSync('cache')){
+    fs.mkdirSync('cache');
+}
+
+for(let i = 4172; i>4162; i--){
+    let res = await axios.get('https://www.qwantz.com/index.php?comic=' + i);
+    const $ = cheerio.load(res.data);
+    let cachePath = `cache/${i}.html`;
+    let data;
+    if(!fs.existsSync(cachePath)){
+        await sleep(2000);
+        let res = await axios.get('https://www.qwantz.com/index.php?comic=' + i);
+        //CACHE
+        fs.writeFileSync(cachePath, res.data);
+        data = res.data;
+        console.log("LIVE REQUEST!");
+    } else {
+        data = fs.readFileSync(cachePath);
+    }
+    
+    let imgSrc = $('td[align="middle"]>img').attr('src');
+    let imgTitle = $('td[align="middle"]>img').attr('title');
+    
+    console.log(imgSrc);
+    console.log(imgTitle);
+    
+
+
+}
